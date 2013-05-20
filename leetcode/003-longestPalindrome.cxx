27de5159048050ec857ee5f4f290bb9886805231
pexeer <pexeer@gmail.com>
2013-05-21 01:17:09 +0800

    003.

---
+++ leetcode/003-longestPalindrome.cxx
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int n = s.size();
+        char map[1002][1002];
+        int a=0, b=0;
+        for (int i=1; i<n; ++i) {
+            for (int j=0; j+i<n; ++j) {
+                if (s[j] == s[j+i]) map[j][j+i] = map[j+1][j+i-1];
+                else map[j][j+i] = 1;
+                if (map[j][j+i]==0) {
+                    a = j;
+                    b = j + i;
+                }
+            } 
+        }
+        return string(s.begin()+a, s.begin()+b+1);
+    }
+};
