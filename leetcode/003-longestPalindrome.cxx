3e5cdd7ebb89c9f1457da291b7eb31029ebbca95
pexeer <pexeer@gmail.com>
2013-05-21 10:14:04 +0800

    Local Array initialized to 0 only for g++ compiler.

--- leetcode/003-longestPalindrome.cxx
+++ leetcode/003-longestPalindrome.cxx
@@ -4,7 +4,7 @@ public:
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
         int n = s.size();
-        char map[1002][1002];
+        char map[1002][1002];   //g++ initialized to 0
         int a=0, b=0;
         for (int i=1; i<n; ++i) {
             for (int j=0; j+i<n; ++j) {
