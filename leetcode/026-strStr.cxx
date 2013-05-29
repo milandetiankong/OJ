9a8c9a17fc97cd6458d10f6a96baed51d9fba60d
pexeer <pexeer@gmail.com>
2013-05-29 23:11:04 +0800

    Time Limit Exceeded.

---
+++ leetcode/026-strStr.cxx
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    char *strStr(char *haystack, char *needle) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        char *p = haystack;
+        char *x, *y;
+        if (*needle==0) return p;
+        while (*p) {
+            x = p;
+            y = needle;
+            while (*x && *y && *x==*y) {
+                ++x;
+                ++y;
+            }
+            if (*y==0) return p;
+            ++p;
+        }
+        return 0;
+    }
+};
