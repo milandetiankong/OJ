32ae761396431c3a1691160247b9599fdabd3948
pexeer <pexeer@gmail.com>
2013-06-13 13:23:50 +0800

    reverse search.

--- leetcode/030-longestValidParentheses.cxx
+++ leetcode/030-longestValidParentheses.cxx
@@ -5,12 +5,12 @@ public:
         // DO NOT write int main() function
         int n = s.size();
         int as = 0;
-        for (int i=0; i<n; ++i) {
+        for (int i=1; i<n; ++i) {
             int tmp = 0;
-            for (int j=i; j<n; ++j) {
-               if (s[j] == '(') ++tmp;  else  --tmp; 
+            for (int j=i; j>=0; --j) {
+               if (s[j] == '(') --tmp;  else  ++tmp; 
                if (tmp < 0) break;
-               if (!tmp && as<(j-i+1)) as = j-i+1;
+               if (!tmp && as<(i-j+1)) as = i-j+1;
             }
         }
         return as;
