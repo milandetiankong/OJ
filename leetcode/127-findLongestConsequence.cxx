2e130649d51e396cd23ea8aacd6532610194ff89
pexeer <pexeer@gmail.com>
2013-10-22 10:56:57 +0800

    hash, scan twice.

--- leetcode/127-findLongestConsequence.cxx
+++ leetcode/127-findLongestConsequence.cxx
@@ -27,3 +27,28 @@ public:
         return maxCt;
     }
 };
+
+class Solution {
+public:
+    int longestConsecutive(vector<int> &num) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        unordered_set<int> s;
+        int ret = 0;
+        for (auto i : num) s.insert(i);
+        for (auto i : num) {
+            int ct = 1;
+            int tmp = i;
+            while (s.find(--tmp) != s.end()) {
+                ++ ct;
+                s.erase(tmp);
+            }
+            tmp = i;
+            while (s.find(++tmp) != s.end()) {
+                ++ ct;
+                s.erase(tmp);
+            }
+            if (ret < ct) ret = ct;
+        }
+        return ret;
+    }
+};
