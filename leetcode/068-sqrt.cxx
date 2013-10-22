8c97b185797abd52946a2bdcf2cc548d75352ee1
pexeer <pexeer@gmail.com>
2013-10-22 10:37:16 +0800

    two sorted Array, find kth elements.

--- leetcode/068-sqrt.cxx
+++ leetcode/068-sqrt.cxx
@@ -3,7 +3,7 @@ public:
     int sqrt(int x) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        if (x<=0) return 0;
+        if (x <= 0) return 0;
         double ret = x;
         double tmp = x - 10;
         while (abs(ret-tmp) >= 1e-3) {
