2e8596675d6a15f1f6bfcc332fa973ddc7e7d248
pexeer <pexeer@gmail.com>
2013-10-28 10:02:53 +0800

    connect head and tail, make a pool.

--- leetcode/059-rotateRight.cxx
+++ leetcode/059-rotateRight.cxx
@@ -10,24 +10,17 @@ class Solution {
 public:
     ListNode *rotateRight(ListNode *head, int k) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
-        if (head == 0) return 0;
+        if (head==0 || k==0) return 0;
         int len = 1;
         ListNode * p = head;
         while (p->next) {
             ++len;
             p = p->next;
         }
-        k = k % len;
-        if (k == 0) return head;
-
         p->next = head;
-        k = len - k;
+        k = len - k % len;
 
-        len = 0;
-        p = head;
-        while (p) {
-            ++len;
-            if (len == k) break;
+        for (int i = 0; i < k; ++i) {
             p = p->next;
         }
         head = p->next;
